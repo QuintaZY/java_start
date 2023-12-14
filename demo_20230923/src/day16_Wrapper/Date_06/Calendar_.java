283230ba7b32be6a48a7f76b8b02d26448ea8624
MYLAPTOP\24556 <2455636304@qq.com>
2023-12-14 23:44:23 +0800

    day18_commit wrapper_ Date

---
+++ demo_20230923/src/day16_Wrapper/Date_06/Calendar_.java
@@ -0,0 +1,20 @@
+package day16_Wrapper.Date_06;
+
+import java.util.Calendar;
+
+/**
+ * @version 1.0
+ * @auther zhangyx
+ */
+public class Calendar_ {
+    public static void main(String[] args) {
+        /*
+        * 1,抽象类   并且构造七是private
+        * 2,可以通过 getInstances() 来获取实例
+        * */
+        Calendar c = Calendar.getInstance();
+//        System.out.println("c=" + c);
+
+    }
+
+}
